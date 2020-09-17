95b90b74eca9470c4c2d2ca28b0b30014dfb19d8
Joshua Lewis <jlewis@bitpay.com>
2020-09-17 10:47:43 -0400

    ipn updates

--- modules/gateways/bitpaycheckout.php
+++ modules/gateways/bitpaycheckout.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * BitPay Checkout 3.0.1.6
+ * BitPay Checkout 3.0.1.7
  *
  * Within the module itself, all functions must be prefixed with the module
  * filename, followed by an underscore, and then the function name. For this
@@ -54,7 +54,7 @@ function bitpaycheckout_MetaData()
 {
     return array(
         'DisplayName' => 'BitPay_Checkout_WHCMS',
-        'APIVersion' => '3.0.1.6', // Use API Version 1.1
+        'APIVersion' => '3.0.1.7', // Use API Version 1.1
         'DisableLocalCreditCardInput' => false,
         'TokenisedStorage' => false,
     );
